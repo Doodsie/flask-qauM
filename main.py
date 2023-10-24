108bd2303cf25efcc69f4aa67ee185aa7069ab99
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-10-25 00:46:00 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask, jsonify
-import os
+# This is a sample Python script.
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
